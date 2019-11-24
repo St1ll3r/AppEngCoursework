f16e8fa060540c32e70a6303ae1d1fa5deacfffb
St1ll3r <up888470@myport.ac.uk>
2019-11-24 23:10:15 +0000

    create/count/setID of lines + fixes/adjustments

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
+insertLine();
+
+
 window.mainTextArea.onkeydown = (event) => {
-  console.log(event.code,event.ctrlKey);
   if(event.code === "Enter") { /* disable paragraph */
+    insertLine();
+    event.preventDefault();
+    return false;
+  }
+  else if(event.code === "Tab" && event.ctrlKey) { /* disable underline */
     return false;
   }
   else if(event.code === "KeyB" && event.ctrlKey) { /* disable bold */
@@ -13,3 +20,17 @@ window.mainTextArea.onkeydown = (event) => {
     return false;
   }
 };
+
+function insertLine(){
+  let element = document.createElement("div");
+  element.setAttribute("class", "line");
+  element.setAttribute("contenteditable", "true");
+  element.setAttribute("id", document.getElementById('mainTextArea').childElementCount);
+  document.getElementById('mainTextArea').appendChild(element);
+  element.focus();
+  updateLineCount();
+}
+
+function updateLineCount(){
+  document.getElementById('lineCount').textContent = document.getElementById('mainTextArea').childElementCount;
+}
