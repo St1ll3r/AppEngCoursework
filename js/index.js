dd32dfd03994a69cee177a939d723b15140c2646
St1ll3r <up888470@myport.ac.uk>
2019-11-25 18:11:28 +0000

    create outline with correct html layout

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 insertLine();
 
-
 window.mainTextArea.onkeydown = (event) => {
   if(event.code === "Enter") { /* disable paragraph */
     insertLine();
     event.preventDefault();
     return false;
   }
-  else if(event.code === "Tab" && event.ctrlKey) { /* disable underline */
+  else if(event.code === "Tab") { /* disable underline */
+    createOutline();
     return false;
   }
   else if(event.code === "KeyB" && event.ctrlKey) { /* disable bold */
@@ -21,6 +21,8 @@ window.mainTextArea.onkeydown = (event) => {
   }
 };
 
+/* let focusedElement = document.activeElement */
+
 function insertLine(){
   let element = document.createElement("div");
   element.setAttribute("class", "line");
@@ -33,4 +35,19 @@ function insertLine(){
 
 function updateLineCount(){
   document.getElementById('lineCount').textContent = document.getElementById('mainTextArea').childElementCount;
+}
+
+function createOutline(){ /* needs optimization */
+  let focusedElement = document.activeElement;
+  let expandElement = document.createElement("div");
+  let outlineDiv = document.createElement("div");
+  expandElement.append("+");
+  expandElement.classList.add("icon", "outlineContent");
+  focusedElement.classList.add("outlineContent");
+  outlineDiv.classList.add("outline");
+  outlineDiv.setAttribute("id", "0"); /* change later, number of outlines */
+  document.getElementById('mainTextArea').insertBefore(expandElement, focusedElement);
+  document.getElementById('mainTextArea').insertBefore(outlineDiv, expandElement);
+  outlineDiv.appendChild(expandElement);
+  outlineDiv.appendChild(focusedElement);
 }
