6501ecab21185f8094e3cce98f5eb8457a7c50d7
St1ll3r <up888470@myport.ac.uk>
2019-12-11 02:00:08 +0000

    fixed node server, now runs on npm start

--- js/init.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/hello', function (req, res) {
-  res.send('Hello ' + (req.query.name || 'anonymous') + '!');
-});
-  
-app.listen(8080);
